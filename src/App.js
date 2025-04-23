1a2d736f9ce8ee2234ae637868b53c482636ec74
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-04-23 23:33:03 +0530

    Added healthcare.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Healthcare from './Healthcare';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Healthcare/>
     </div>
   );
 }
